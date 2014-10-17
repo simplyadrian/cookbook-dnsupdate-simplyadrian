0877ad0e3489ad4e870764dd21f8688ca5bc9bd1
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2014-10-17 14:15:25 -0500

    Moved cookbook nativex-dnsupdate to dnsupdate-nativex

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: nativex-dnsupdate
+# Recipe:: default
+#
+# Copyright 2014, NativeX
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "nativex-dnsupdate::addpub"
+include_recipe "nativex-dnsupdate::addprv"
